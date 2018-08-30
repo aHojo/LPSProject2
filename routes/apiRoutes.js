fac079e9ef2e87b2df3602530dde445dc4147c72
amh5366 <amh5366@upenn.edu>
2018-08-30 18:39:20 -0400

    api stuff

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,24 +3,24 @@ var express = require("express");
 var router = express.Router();
 var twilio = require('twilio');
 
-var returnRouter = function(io){
-  router
-    .post("/sendMessage", function(req, res) {
-      var message = req.body
-      var name = req.body.name;
-      console.log(name);
-      console.log(message);
-      res.json(message);
-    })
-    .post("/sms", function(req,res) {
-      console.log(req.body);
-      var messageBody = req.body.Body;
-      var from = req.body.From;
-      console.log(messageBody);
-      io.emit('text', {messageBody, from});
-      res.send('Event received');
-    });
+var returnRouter = function (io) {
+	router
+		.post("/sendMessage", function (req, res) {
+			var message = req.body
+			var name = req.body.name;
+			console.log(name);
+			console.log(message);
+			res.json(message);
+		})
+		.post("/sms", function (req, res) {
+			console.log(req.body);
+			var messageBody = req.body.Body;
+			var from = req.body.From;
+			console.log(messageBody);
+			io.emit('text', { messageBody, from });
+			res.send('Event received');
+		});
 
-    return router;
+	return router;
 }
 module.exports = returnRouter;
